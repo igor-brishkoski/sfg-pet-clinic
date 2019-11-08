0cfac685b6d70d195eae14a05a9d85045b477c1a
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-07 22:44:51 -0500

    Add Model services Resolves #5

---
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package brishko.com.sfgpetclinic.services;
+
+import brishko.com.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet owner);
+    Set<Pet> findAll();
+}
