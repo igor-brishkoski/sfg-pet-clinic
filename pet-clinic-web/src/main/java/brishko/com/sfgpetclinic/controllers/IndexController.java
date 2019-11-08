8544cf2edd8b530901bd05fc99672691145b0e8e
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-08 17:36:48 -0500

    Implement map services Resolves #6

---
+++ pet-clinic-web/src/main/java/brishko/com/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package brishko.com.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+    // maps all those paths to index.html
+    // value is alias for path
+    @RequestMapping(path = {"", "/", "index", "index.html"}, method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
