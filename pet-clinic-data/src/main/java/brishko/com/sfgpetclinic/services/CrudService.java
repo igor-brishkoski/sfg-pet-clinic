a442ee4b658abdf97c7790efc7c6bc0c1927a41e
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-08 17:03:08 -0500

    Refactor service interfaces Resolves #12

---
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/CrudService.java
@@ -0,0 +1,15 @@
+package brishko.com.sfgpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T Save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/PetService.java
@@ -2,12 +2,5 @@ package brishko.com.sfgpetclinic.services;
 
 import brishko.com.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
