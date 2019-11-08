084cfd833132f5c3dbcad09aae278087b731f97a
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-08 16:50:01 -0500

    Additional comments for gradle implementation

--- pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/PetService.java
@@ -6,6 +6,8 @@ import java.util.Set;
 
 public interface PetService {
     Pet findById(Long id);
+
     Pet save(Pet owner);
+
     Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/VetService.java
@@ -6,7 +6,9 @@ import java.util.Set;
 
 public interface VetService {
     Vet findById(Long id);
+
     Vet save(Vet owner);
+
     Set<Vet> findAll();
 
 }
