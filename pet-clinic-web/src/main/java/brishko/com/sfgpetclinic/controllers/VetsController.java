1bea7b31696d83a4d3f7f7bab2071ae302396dd7
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-09 15:09:54 -0500

    Display List of vets

--- pet-clinic-web/src/main/java/brishko/com/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/brishko/com/sfgpetclinic/controllers/VetsController.java
@@ -1,13 +1,22 @@
 package brishko.com.sfgpetclinic.controllers;
 
+import brishko.com.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetsController {
 
+    private VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping(path = {"/vets", "vets/index", "vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
