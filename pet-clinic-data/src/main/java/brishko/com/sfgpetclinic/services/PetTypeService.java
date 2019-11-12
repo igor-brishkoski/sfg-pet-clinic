ea0e7c9760b5429fe8fe534b66b48c09348be378
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-12 17:14:11 -0500

    Add pettypeservice and bootstrap data

---
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package brishko.com.sfgpetclinic.services;
+
+import brishko.com.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
--- pet-clinic-web/src/main/java/brishko/com/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/brishko/com/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package brishko.com.sfgpetclinic.bootstrap;
 
 import brishko.com.sfgpetclinic.model.Owner;
+import brishko.com.sfgpetclinic.model.PetType;
 import brishko.com.sfgpetclinic.model.Vet;
 import brishko.com.sfgpetclinic.services.OwnerService;
+import brishko.com.sfgpetclinic.services.PetTypeService;
 import brishko.com.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,15 +17,40 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        //region Owner Data
+        createPetTypes();
+        createOwners();
+        createVets();
+    }
+
+    //region Description
+    private void createPetTypes() {
+        PetType p1 = new PetType();
+        p1.setName("Dog");
+        petTypeService.save(p1);
+
+        PetType p2 = new PetType();
+        p2.setName("Cat");
+        petTypeService.save(p2);
+
+        PetType p3 = new PetType();
+        p3.setName("Bird");
+        petTypeService.save(p3);
+    }
+    //endregion
+
+    //region Owner Data
+    private void createOwners() {
+
         Owner owner = new Owner();
         owner.setFirstName("John");
         owner.setLastName("Snow");
@@ -44,11 +71,13 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner2);
         ownerService.save(owner3);
         ownerService.save(owner4);
-        //endregion
 
         System.out.println("Saving Owners data...");
+    }
+    //endregion
 
-        //region Vet Data
+    //region Vet Data
+    private void createVets() {
         Vet vet = new Vet();
         vet.setFirstName("Jack");
         vet.setLastName("Sparrow");
@@ -64,8 +93,8 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet);
         vetService.save(vet2);
         vetService.save(vet3);
-        //endregion
 
         System.out.println("Saving Vets data...");
     }
+    //endregion
 }
