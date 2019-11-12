171e737a722d94851a78243fe645fd03222b79f3
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-12 17:00:45 -0500

    Add specalty

---
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,13 @@
+package brishko.com.sfgpetclinic.model;
+
+public class Specialty extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
