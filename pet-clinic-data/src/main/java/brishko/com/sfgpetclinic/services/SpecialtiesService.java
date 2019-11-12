46144248bd889295ab54a94b0866a478040923bf
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-11-12 17:51:53 -0500

    Specialty service

---
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package brishko.com.sfgpetclinic.services;
+
+import brishko.com.sfgpetclinic.model.Specialty;
+import brishko.com.sfgpetclinic.services.CrudService;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
