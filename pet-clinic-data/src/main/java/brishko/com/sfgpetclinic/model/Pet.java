520f1a61395e1d5dba5b0c03f3b02e799b73f539
Igor Brishkoski <igor-brishkoski@gmail.com>
2019-12-10 17:42:42 -0500

    Add visit jpa mapping

--- pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/brishko/com/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package brishko.com.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -14,6 +16,8 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     private LocalDate birthDate;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public PetType getPetType() {
         return petType;
